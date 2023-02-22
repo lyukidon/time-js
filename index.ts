921347399a6058242c2aa1d3f0c3a525c9c4e0aa
lyukidon <qgx99098@gmail.com>
2023-02-22 16:24:53 +0900

    remove files

--- index.ts
+++ index.ts
@@ -1,4 +1,39 @@
-import Date from './date.js'
-import Time from './time.js'
+import userFormat from "./utils/userFormat.js";
+import { manageStr, combine } from './utils/functions.js'
+import {year, month, day} from './utils/dateData.js'
+import { hour, minute, second } from "./utils/dateData.js";
 
-export {Date, Time}
+export const Time = (form: string, militaryTime?: boolean) => {
+    let time: string[] = [];
+    const format = userFormat(form);
+    if (format.H) {
+        if (militaryTime) {
+            time = [...time, manageStr(hour(militaryTime) + "", format.H)];
+        } else {
+            time = [...time, manageStr(hour()+"", format.H)]
+        }
+    }
+    if (format.M) {
+        time = [...time, manageStr(minute() + "", format.M)];
+    }
+    if (format.S) {
+        time = [...time, manageStr(second() + "", format.S)];
+    }
+    return combine(time, format.mark);
+};
+
+
+export const Date = (form : string) => {
+    let date : string[] = [];
+    const format = userFormat(form);
+    if (format.Y) {
+        date = [...date, manageStr(year() + "", format.Y)];
+    }
+    if (format.M) {
+        date = [...date, manageStr(month() + "", format.M)];
+    }
+    if (format.D) {
+        date = [...date, manageStr(day() + "", format.D)];
+    }
+	return combine(date,format.mark);
+};
